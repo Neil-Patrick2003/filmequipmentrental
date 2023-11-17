19b4119e3cf9a0dbccb85205ee371f6b71e54d9d
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2023-11-18 02:41:46 +0800

    add uuid

--- src/main/java/com/mycompany/filmequipmentrentalservice/TransactionService.java
+++ src/main/java/com/mycompany/filmequipmentrentalservice/TransactionService.java
@@ -5,9 +5,14 @@
 package com.mycompany.filmequipmentrentalservice;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  *
@@ -19,21 +24,69 @@ public class TransactionService {
     private static final String ID_COLUMN = "id";
     private static final String START_DATE_COLUMN = "start_date";
     private static final String END_DATE_COLUMN = "end_date";
-    private static final String CUSTOMER_ID_COLUMNS = "customer_id";
+    private static final String CUSTOMER_ID_COLUMN = "customer_id";
     private static final String STATUS_COLUMN = "status";
     private static final String TOTAL_COLUMN = "total";
 
+    public static List getAllTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+
+        Connection conn = AccessDatabaseConnector.connect();
+        try {
+            Statement statement = conn.createStatement();
+
+            String selectQuery = "SELECT "
+                    + "*, "
+                    + "customers.name AS customer_name, "
+                    + "customers.email AS customer_email, "
+                    + "customers.username AS customer_username, "
+                    + "customers.address AS customer_address, "
+                    + "customers.phone_number AS customer_phone_number "
+                    + "FROM transactions "
+                    + "LEFT JOIN customers ON transactions.customer_id = customers.id";
+
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+            // Process the results
+            while (resultSet.next()) {
+
+                UUID id = UUID.fromString(resultSet.getString(ID_COLUMN));
+                Date startDate = dateFormatter.parse(resultSet.getString(START_DATE_COLUMN));
+                Date endDate = dateFormatter.parse(resultSet.getString(END_DATE_COLUMN));
+                int customerId = resultSet.getInt(CUSTOMER_ID_COLUMN);
+                Double total = resultSet.getDouble(TOTAL_COLUMN);
+                String status = resultSet.getString(STATUS_COLUMN);
+
+                Transaction transaction = new Transaction(id, startDate, endDate, customerId, status, total, new ArrayList<TransactionItem>());
+                transactions.add(transaction);
+            }
+
+            // Close the result set and statement
+            resultSet.close();
+            statement.close();
+
+            return transactions;
+        } catch (Exception e) {
+            System.out.print(e);
+        } finally {
+            AccessDatabaseConnector.closeConnection(conn);
+        }
+
+        return null;
+    }
+
     public static void saveTransaction(Transaction transaction) {
         Connection conn = AccessDatabaseConnector.connect();
-        
+
         SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 
-  
         try {
             // Execute an INSERT query
             try (Statement statement = conn.createStatement()) {
                 // Execute an INSERT query
-                String insertQuery = "INSERT INTO " + TRANSACTIONS_TABLE + " (" + ID_COLUMN + ", " + START_DATE_COLUMN + ", " + END_DATE_COLUMN + ", " + CUSTOMER_ID_COLUMNS + ", " + STATUS_COLUMN + ", " + TOTAL_COLUMN + " ) VALUES ('" + transaction.id.toString() + "', '"  + dateFormatter.format(transaction.startDate) + "', '" + dateFormatter.format(transaction.endDate) + "', '" + transaction.customer_id + "', '" + transaction.status + "', '" + transaction.total.toString() + "');";
+                String insertQuery = "INSERT INTO " + TRANSACTIONS_TABLE + " (" + ID_COLUMN + ", " + START_DATE_COLUMN + ", " + END_DATE_COLUMN + ", " + CUSTOMER_ID_COLUMN + ", " + STATUS_COLUMN + ", " + TOTAL_COLUMN + " ) VALUES ('" + transaction.id.toString() + "', '" + dateFormatter.format(transaction.startDate) + "', '" + dateFormatter.format(transaction.endDate) + "', '" + transaction.customer_id + "', '" + transaction.status + "', '" + transaction.total.toString() + "');";
                 System.out.println(insertQuery);
                 int rowsAffected = statement.executeUpdate(insertQuery);
                 // Check the number of rows affected
